24da1d54a81a3c787e273a8c26cb7e0405b45f66
unknown <charlestoochukwu5@gmail.com>
2021-09-29 21:28:09 +0100

    database for project list added

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -19,6 +19,8 @@ namespace ProjectAllocationSystem.Data
 
         public DbSet<LecturerStudentNode> LecturerStudentNodes { get; set; }
 
+        public DbSet<Project> Projects { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
